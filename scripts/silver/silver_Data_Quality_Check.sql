8cb4fceab6c2d6b58981e3581fef5ec1b0573ff8
Ketan <ketandimble10@gmail.com>
2025-06-03 11:58:20 +0530

    silver layer update

--- scripts/silver/silver_Data_Quality_Check.sql
+++ scripts/silver/silver_Data_Quality_Check.sql
@@ -7,7 +7,6 @@ from silver.crm_cust_info
 group by cst_id
 having count(*) > 1 or cst_id is null;
 
-
 select 
 prd_id,
 count(*)
@@ -20,16 +19,25 @@ having count(*) > 1 or prd_id is null;
 
 select cst_firstname
 from silver.crm_cust_info
-where cst_firstname != TRIM(cst_firstname)
+where cst_firstname != TRIM(cst_firstname);
 
-select cst_lastname
+
+select cst_firstname
 from silver.crm_cust_info
-where cst_lastname != TRIM(cst_lastname)
+where cst_firstname != TRIM(cst_firstname);
 
 
 select prd_nm
 from silver.crm_prd_info
 where prd_nm != TRIM(prd_nm);
+
+select sls_ord_num
+from silver.crm_sales_details
+where sls_ord_num != TRIM(sls_ord_num);
+
+SELECT * FROM silver.erp_px_cat_g1v2
+where cat != TRIM(cat) OR subcat != TRIM(subcat) OR maintenance != TRIM(maintenance)
+
 
 --- Data standardization and Consistency
 
@@ -42,6 +50,15 @@ from silver.crm_cust_info;
 
 select distinct prd_line
 from silver.crm_prd_info;
+
+SELECT DISTINCT
+gen
+FROM silver.erp_cust_az12
+
+
+SELECT DISTINCT
+ subcat 
+FROM silver.erp_px_cat_g1v2
 
 
 
@@ -59,3 +76,58 @@ prd_start_dt,
 prd_end_dt
 FROM silver.crm_prd_info
 WHERE prd_end_dt < prd_start_dt;
+
+
+
+
+SELECT * FROM silver.crm_sales_details
+where sls_cust_id NOT IN (SELECT cst_id FROM silver.crm_cust_info)
+
+
+-- Check for Invalid Dates
+
+SELECT sls_order_dt 
+FROM silver.crm_sales_details
+WHERE sls_order_dt <= 0 
+OR LEN(sls_order_dt) !=8  
+OR sls_order_dt > 20500101
+OR sls_order_dt < 1900101
+
+
+SELECT sls_ship_dt 
+FROM silver.crm_sales_details
+WHERE sls_ship_dt <= 0 
+OR LEN(sls_ship_dt) !=8  
+OR sls_ship_dt > 20500101
+OR sls_ship_dt < 1900101
+
+
+--- Check for Invalid Date Orders
+
+SELECT *
+FROM silver.crm_sales_details
+where sls_order_dt > sls_ship_dt OR sls_order_dt > sls_ship_dt
+
+
+-- Check data consistency betwwen sales, quantity and price
+
+SELECT DISTINCT
+sls_sales,
+sls_quantity,
+sls_price
+FROM silver.crm_sales_details
+where sls_sales != sls_quantity*sls_price
+OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
+OR sls_sales <=0 OR sls_quantity <=0  OR sls_price <= 0
+ORDER BY sls_sales,sls_quantity,sls_price
+
+
+
+---Identify Out-Of-Range Dates
+
+SELECT DISTINCT
+bdate
+from silver.erp_cust_az12
+where bdate < '1924-01-01' OR  bdate > GETDATE()
+
+
